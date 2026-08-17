bab027f91105d76786ea01b2b2c10a8a4db9b23d
Tugbacevikk <tcevik2824@gmail.com>
2026-08-17 15:15:41 +0300

    Set OpenCV CAP_PROP_BUFFERSIZE=1 for 0-ms live latency

--- core/camera_manager.py
+++ core/camera_manager.py
@@ -359,6 +359,8 @@ class CameraProcessor:
                         if system != 'Windows':
                             try:
                                 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+                                cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+                                cap.set(cv2.CAP_PROP_FPS, 30)
                             except Exception:
                                 pass
                         ret_test, frame_test = cap.read()
